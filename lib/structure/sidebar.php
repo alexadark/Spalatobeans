bee1e000ff00418c0f052f35278a7d7e157e7dc4
Alexa Spalato <alexaspalato@gmail.com>
2017-11-25 18:06:52 +0100

    sidebar width

--- lib/structure/sidebar.php
+++ lib/structure/sidebar.php
@@ -9,4 +9,8 @@ add_action( 'wp', 'wst_set_up_sidebars_structure' );
  */
 function wst_set_up_sidebars_structure() {
 	beans_add_attribute( 'beans_widget_panel', 'class', 'uk-panel-box' );
+
+	 //change sidebar width
+	beans_replace_attribute('beans_primary', 'class', 'uk-width-medium-3-4','uk-width-large-7-10');
+	beans_replace_attribute('beans_sidebar_primary', 'class', 'uk-width-medium-1-4','uk-width-large-3-10');
 }
