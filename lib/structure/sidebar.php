64b594580b9e0c4ae654b7d69324cfa79091cb54
Alexa Spalato <alexaspalato@gmail.com>
2017-11-29 16:47:22 +0100

    layout

--- lib/structure/sidebar.php
+++ lib/structure/sidebar.php
@@ -1,5 +1,5 @@
 <?php
-add_action( 'wp', 'wst_set_up_sidebars_structure' );
+//add_action( 'wp', 'wst_set_up_sidebars_structure' );
 /**
  * Set_up_sidebars_structure
  *
@@ -9,15 +9,28 @@ add_action( 'wp', 'wst_set_up_sidebars_structure' );
  */
 function wst_set_up_sidebars_structure() {
 	beans_add_attribute( 'beans_widget_panel', 'class', 'uk-panel-box' );
-
-	 //change sidebar width
+	//change sidebar width
 	beans_replace_attribute('beans_primary', 'class', 'uk-width-medium-3-4','uk-width-large-7-10 uk-width-medium-2-3');
 	beans_replace_attribute('beans_sidebar_primary', 'class', 'uk-width-medium-1-4','uk-width-large-3-10 uk-width-medium-1-3');
-
 //	if(is_page_template())
+}
 
+
+add_action( 'widgets_init', 'wst_remove_secondary_sidebar' );
+/**
+ * Unregister secondary sidebar
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+
+function wst_remove_secondary_sidebar() {
+
+	beans_deregister_widget_area( 'sidebar_secondary' );
 
 }
+
 
 
 
